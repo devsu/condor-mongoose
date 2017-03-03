8feae57adfbc1cc2f61d372d35496601d1ec4365
Jorge Arindia <jat665@gmail.com>
2017-03-03 10:27:45 -0500

    refactor

--- spec/stubs/models/sample/sample-service.js
+++
@@ -1,8 +0,0 @@
-const Model = require('./sample-model');
-const BaseService = require('../../../../lib/base-service');
-
-module.exports = class extends BaseService {
-  constructor() {
-    super(Model);
-  }
-};
